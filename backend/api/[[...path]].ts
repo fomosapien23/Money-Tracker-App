e1a5a609a1c22c66b8d5235748de630ec2ddf620
rohanrai230902 <rohanrai230902@gmail.com>
2026-04-04 22:48:12 +0530

    4 Add  vercel config

--- backend/api/[[...path]].ts
+++ backend/api/[[...path]].ts
@@ -1,4 +1,3 @@
-import serverless from "serverless-http";
-import { app } from "../src/app";
+import { app } from "./lib/app.js";
 
-export default serverless(app);
+export default app;
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,4 +1,4 @@
-import { app } from "./app.js";
+import { app } from "../api/lib/app.js";
 
 const port = Number(process.env.PORT ?? 4000);
 
